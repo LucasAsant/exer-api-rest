07c3974da326339119034cb360f340870bbdae9d
Welerson <luizpawlak7@gmail.com>
2019-05-24 14:13:19 -0300

    fechando arquivo de log

--- src/main/java/hello/Embarcador.java
+++ src/main/java/hello/Embarcador.java
@@ -4,50 +4,52 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.logging.*;
+import java.io.*;
 
 @RestController
 @RequestMapping(value = "embarcadores")
 public class Embarcador {
 
-    @CrossOrigin
     @PostMapping()
     public String create() {
+        loger("create");
         return "{create}";
     }
 
-    @CrossOrigin
     @GetMapping()
     public String read() {
-
-	    Logger logger = Logger.getLogger("MyLog");
-	    FileHandler fh;
-
-        try {
-			fh = new FileHandler("my-log-file.log");
-	        logger.addHandler(fh);
-	        SimpleFormatter formatter = new SimpleFormatter();
-	        fh.setFormatter(formatter);
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-
-		logger.info("{read}");
+        loger("read");
         return "{read}";
     }
 
-    @CrossOrigin
     @PutMapping()
     public String update() {
+        loger("update");
         return "{update}";
     }
 
-    @CrossOrigin
     @DeleteMapping()
     public String delete() {
+        loger("delete");
         return "{delete}";
+    }
+
+    private void loger(String request) {
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("my-log-file.log", true);
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            logger.info(request);
+            fh.close();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
