c13454a2551789f59136abb29ad90a5a015ac57f
Welerson <luizpawlak7@gmail.com>
2019-05-31 12:05:41 -0300

    Adição de operações com o MongoDB para cada requisição.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,10 @@
 package hello;
 
-import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.log4j.*;
 
 @SpringBootApplication
 public class Application {
-
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/hello/Embarcador.java
+++ src/main/java/hello/Embarcador.java
@@ -1,40 +1,88 @@
 package hello;
 
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.*;
-import java.io.*;
 
 @RestController
 @RequestMapping(value = "embarcadores")
 public class Embarcador {
 
-    @PostMapping()
-    public String create() {
-        loger("create");
-        return "{create}";
-    }
+    private String databaseName = "Examples";                                   //Choosing Database
+    private MongoClient mongoClient = new MongoClient();                        //Creating connection to the Database
+    private MongoDatabase database = mongoClient.getDatabase(databaseName);     //Connecting to the Database
 
-    @GetMapping()
+    private MongoCollection<Document> collectionName = database.getCollection("embarcadores");    //Choosing the collection
+    private List<String> placasEmbarcador = Arrays.asList("ABC1234", "ABC1235");                     //Example array
+
+    @GetMapping()   //Get Request reading a collection
     public String read() {
-        loger("read");
+
+        FindIterable<Document> findIterable = collectionName.find();    //Selecting the collection
+        Iterator<Document> iterator = findIterable.iterator();          //Creating variable iterator to run through all the lines selected
+
+        iterator.forEachRemaining(System.out::println);                 //Printing all lines selected
+
+        logger("read"); //Logging operation;
         return "{read}";
     }
 
-    @PutMapping()
+    @PostMapping() //Post Request creating a new document in the collection
+    public String create() {
+
+        Document document = new Document();     //Creating the document
+        ObjectId id = new ObjectId();           //Creating a new random Id;
+
+        /*  Imputing data in the document  */
+        document.put("_id", id.toString());
+        document.put("name", "Nome Original");
+        document.append("address", new Document("street", "123 Fake St"));
+        document.put("city", "Faketon");
+        document.put("state", "MA");
+        document.put("zip", 12345);
+        document.put("cars", placasEmbarcador);
+
+        collectionName.insertOne(document);     //Inserting the document in the collection
+
+        logger("create"); //Logging operation;
+        return "{create}";
+    }
+
+    @PutMapping() //Put Request updating a document in the collection
     public String update() {
-        loger("update");
+
+        Document updateFields = new Document();         //Creating the update document
+        updateFields.append("name", "Nome Alterado");   //Setting new values in the update document
+
+        Document setQuery = new Document("$set", updateFields);     //Creating document to set the new values
+        Document searchQuery = new Document("name", "Nome Original");   //Creating document to search the field to be altered
+
+        collectionName.updateMany(searchQuery,setQuery); //Updating the document searched with the new values
+
+        logger("update"); //Logging operation;
         return "{update}";
     }
 
-    @DeleteMapping()
+    @DeleteMapping() //Delete Request deleting a collection
     public String delete() {
-        loger("delete");
+
+        collectionName.drop();      //Deleting the collection embarcadores
+        logger("delete");   //Logging operation;
         return "{delete}";
     }
 
-    private void loger(String request) {
+    //Logger method
+    private void logger(String request) {
         Logger logger = Logger.getLogger("MyLog");
         FileHandler fh;
 
