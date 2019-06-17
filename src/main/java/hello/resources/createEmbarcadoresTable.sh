fe9a90062a6ed05ad330b061e0f8002ad7d86502
Welerson <luizpawlak7@gmail.com>
2019-06-17 16:26:12 -0300

    corrigindo createTable

--- src/main/java/hello/resources/createEmbarcadoresTable.sh
+++ src/main/java/hello/resources/createEmbarcadoresTable.sh
@@ -3,7 +3,7 @@ aws dynamodb create-table ^
     --table-name Embarcadores ^
     --attribute-definitions ^
         AttributeName=id,AttributeType=S ^
-        AttributeName=nome,AttributeType=S ^
-    --key-schema AttributeName=id,KeyType=HASH AttributeName=nome,KeyType=RANGE ^
+        AttributeName=placa,AttributeType=S ^
+    --key-schema AttributeName=id,KeyType=HASH AttributeName=placa,KeyType=RANGE ^
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 ^
     --endpoint-url http://localhost:8000
